c926db590a13c800a3eb217aa662dd77375e6207
alikvkli <ali.kavakli4598@gmail.com>
2023-12-30 18:27:26 +0300

    add skeleton animation

--- src/pages/flows/step_1.tsx
+++ src/pages/flows/step_1.tsx
@@ -1,16 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { answerQuestion } from "../../features/autism";
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { setInitialFlow } from "../../features/app";
+import { Skeleton } from "@mui/material";
 
 export default function Step1() {
     const { appName } = useAppSelector(state => state.app);
     const { currentQuestion, questions, done } = useAppSelector(state => state.autism);
     const getQuestion = questions.find(item => item.id === currentQuestion);
     const dispatch = useAppDispatch();
+    const [loading, setLoading] = useState<boolean>(false);
+    const ref = useRef<HTMLDivElement>(null);
 
     const handleAnswer = (answer: string) => {
+        setLoading(true);
+        ref.current?.focus();
         dispatch(answerQuestion(answer))
+        let timeOut = Math.floor(Math.random() * (1000 - 500 + 1)) + 500;
+        setTimeout(() => {
+            setLoading(false)
+        }, timeOut)
     }
 
     useEffect(() => {
@@ -32,24 +41,37 @@ export default function Step1() {
             </div>
             {!done && (
                 <div className="flex flex-col bg-white shadow-md p-4 md:min-h-[300px]  rounded-md w-full items-center justify-center">
-                    <div>
-                        <h4 className="text-sm text-gray-500">Soru {currentQuestion}:</h4>
-                        <p className="mt-2 font-semibold">{getQuestion?.qa}</p>
-                        <div className="flex max-md:flex-col mt-4 items-start justify-start gap-4 w-full">
-                            <button onClick={() => handleAnswer("Kesinlikle katılıyorum")} type="button" className="border-[1px] cursor-pointer max-md:w-full text-sm p-4 rounded-md shadow-md border-[#5068cb] hover:bg-[#5068cb] hover:text-white transition-all">
-                                Kesinlikle katılıyorum
-                            </button>
-                            <button onClick={() => handleAnswer("Sıklıkla katılıyorum")} type="button" className="border-[1px] cursor-pointer max-md:w-full text-sm p-4 rounded-md shadow-md border-[#5068cb] hover:bg-[#5068cb] hover:text-white transition-all">
-                                Sıklıkla katılıyorum
-                            </button>
-                            <button onClick={() => handleAnswer("Bazen katılıyorum")} type="button" className="border-[1px] cursor-pointer max-md:w-full text-sm p-4 rounded-md shadow-md border-[#5068cb] hover:bg-[#5068cb] hover:text-white transition-all">
-                                Bazen katılıyorum
-                            </button>
-                            <button onClick={() => handleAnswer("Kesinlikle katılmıyorum")} type="button" className="border-[1px] cursor-pointer max-md:w-full text-sm p-4 rounded-md shadow-md border-[#5068cb] hover:bg-[#5068cb] hover:text-white transition-all">
-                                Kesinlikle katılmıyorum
-                            </button>
+                    {!loading && (
+                        <div>
+                            <h4 className="text-sm text-gray-500">Soru {currentQuestion}:</h4>
+                            <p className="mt-2 font-semibold">{getQuestion?.qa}</p>
+                            <div className="flex max-md:flex-col mt-4 items-start justify-start gap-4 w-full">
+                                <button onClick={() => handleAnswer("Kesinlikle katılıyorum")} type="button" className="border-[1px] cursor-pointer max-md:w-full text-sm p-4 rounded-md shadow-md border-[#5068cb] hover:bg-[#5068cb] hover:text-white transition-all">
+                                    Kesinlikle katılıyorum
+                                </button>
+                                <button onClick={() => handleAnswer("Sıklıkla katılıyorum")} type="button" className="border-[1px] cursor-pointer max-md:w-full text-sm p-4 rounded-md shadow-md border-[#5068cb] hover:bg-[#5068cb] hover:text-white transition-all">
+                                    Sıklıkla katılıyorum
+                                </button>
+                                <button onClick={() => handleAnswer("Bazen katılıyorum")} type="button" className="border-[1px] cursor-pointer max-md:w-full text-sm p-4 rounded-md shadow-md border-[#5068cb] hover:bg-[#5068cb] hover:text-white transition-all">
+                                    Bazen katılıyorum
+                                </button>
+                                <button onClick={() => handleAnswer("Kesinlikle katılmıyorum")} type="button" className="border-[1px] cursor-pointer max-md:w-full text-sm p-4 rounded-md shadow-md border-[#5068cb] hover:bg-[#5068cb] hover:text-white transition-all">
+                                    Kesinlikle katılmıyorum
+                                </button>
+                            </div>
                         </div>
-                    </div>
+                    )}
+                    {loading && <div className="flex w-full p-4 gap-3 flex-col items-start justify-start">
+                        <Skeleton variant="rectangular" width="30%" height={15} />
+                        <Skeleton variant="rectangular" width="100%" height={15} />
+                        <Skeleton variant="rectangular" width="100%" height={15} />
+
+                        <Skeleton variant="rectangular" width="100%" height={40} />
+                        <Skeleton variant="rectangular" width="100%" height={40} />
+                        <Skeleton variant="rectangular" width="100%" height={40} />
+                        <Skeleton variant="rectangular" width="100%" height={40} />
+
+                    </div>}
                 </div>
             )}
 
